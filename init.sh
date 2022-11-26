7dc747eea2dd39e5cba0ae0762f62f54c47ef85b
zlibd <>
2022-11-26 17:34:21 -0500

    couldnt clean file, keeping proper ansible like

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install ansible
-python3 -m pip install --user ansible
+python3 -m pip -q install --user ansible
 
 # Run the playbook locally
 ~/.local/bin/ansible-playbook -i localhost, -K kali.yml
