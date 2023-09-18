472048bb499f1c6643527c8bf51fb4e17680c491
zlibd <>
2023-09-18 06:34:14 -0400

    updated autorecon

--- init.sh
+++ init.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# Not POSIX compliant
 
-# Get user sudo password
 read -s -p "sudo password: " PASSWORD
 
 # Install ansible
 python3 -m pip -q install --user ansible
 
 # Run the playbook locally
-~/.local/bin/ansible-playbook -i localhost, -K kali.yml
+#~/.local/bin/ansible-playbook -i localhost, -K kali.yml
 ~/.local/bin/ansible-playbook kali.yml -i localhost, -e "ansible_sudo_pass=$PASSWORD"
